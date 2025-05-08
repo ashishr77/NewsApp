62160f72374a4bc5b7a58cb28572b2448c25c8fa
Ashish Rathod <161116389+ashishr77@users.noreply.github.com>
2025-05-09 03:47:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const API_KEY = "96f81d399a5a48e382020853948f62fb';
-const url = https://newsapi.org/v2/everything?q=;
+const url = "https://newsapi.org/v2/everything?q=";
 ;
 
 window.addEventListener("load", () => fetchNews("trending"));
